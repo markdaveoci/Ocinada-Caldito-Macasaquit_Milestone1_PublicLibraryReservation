3ca6dd1d37838fe48cd6b4fced22b191362ddea5
Sean Macasaquit <macasaquitseankevin@gmail.com>
2026-04-06 19:12:58 +0800

    ReservationClass

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,3 +1,35 @@
 public class Reservation {
+    public class Reservation {
+
+        private final int reservationId;
+        private final int patronId;
+        private final int bookId;
+        private final String status;
+
+        public Reservation(int reservationId, int patronId, int bookId, String status) {
+            this.reservationId = reservationId;
+            this.patronId = patronId;
+            this.bookId = bookId;
+            this.status = status;
+        }
+
+        public int getReservationId() {
+            return reservationId;
+        }
+
+        public int getPatronId() {
+            return patronId;
+        }
+
+        public int getBookId() {
+            return bookId;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+    }
+
+
 
 }
